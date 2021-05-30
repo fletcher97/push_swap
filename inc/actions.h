278d9acdb509472701d3558de41ff96164eb272c
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-05-31 00:49:36 +0100

    norm fix

--- inc/actions.h
+++ inc/actions.h
@@ -6,7 +6,7 @@
 /*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/30 05:32:17 by mgueifao          #+#    #+#             */
-/*   Updated: 2021/05/30 06:14:49 by mgueifao         ###   ########.fr       */
+/*   Updated: 2021/05/31 00:49:01 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,16 @@ typedef struct s_ps
 	t_stack	*b;
 }	t_ps;
 
-void pa(t_ps *ps);
-void pb(t_ps *ps);
-void ra(t_ps *ps);
-void rb(t_ps *ps);
-void rr(t_ps *ps);
-void rra(t_ps *ps);
-void rrb(t_ps *ps);
-void rrr(t_ps *ps);
-void sa(t_ps *ps);
-void sb(t_ps *ps);
-void ss(t_ps *ps);
+void	pa(t_ps *ps);
+void	pb(t_ps *ps);
+void	ra(t_ps *ps);
+void	rb(t_ps *ps);
+void	rr(t_ps *ps);
+void	rra(t_ps *ps);
+void	rrb(t_ps *ps);
+void	rrr(t_ps *ps);
+void	sa(t_ps *ps);
+void	sb(t_ps *ps);
+void	ss(t_ps *ps);
 
 #endif
