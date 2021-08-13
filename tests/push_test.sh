ddbcba2cafd84c017a0153deb3d84f8b28237f44
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-08-13 23:15:15 +0100

    Made script run in paralle

--- tests/push_test.sh
+++ tests/push_test.sh
@@ -1,28 +1,41 @@
-max=0
-for i in {1..100};
-do
-	COUNT=$2
-	LIMIT=$3
+testps () {
+	COUNT=$3
+	LIMIT=$4
 	ARG=`seq $COUNT | shuf | paste -sd " "`
 	tot=`$1 $ARG | wc -l`
-	if [ $max -eq 0 ];
+	check=`$1 $ARG | $2 $ARG`
+	out=""
+	if [ $check != "OK" ];
 	then
-		min=$tot
+		out="Checker: KO\n $ARG"
+	else
+		out="Checker: OK"
 	fi
-	if [ $min -gt $tot ];
-	then
-		min=$tot
-	fi
-	if [ $max -lt $tot ];
-	then
-		max=$tot
-	fi
+	# if [ $max -eq 0 ];
+	# then
+	# 	min=$tot
+	# fi
+	# if [ $min -gt $tot ];
+	# then
+	# 	min=$tot
+	# fi
+	# if [ $max -lt $tot ];
+	# then
+	# 	max=$tot
+	# fi
 	if [ $tot -gt $LIMIT ];
 	then
-		echo "Over $LIMIT - Total: $tot - exceeded by: $(( $tot - $LIMIT ))"
+		echo -e "$out\nOver $LIMIT - Total: $tot - exceeded by: $(( $tot - $LIMIT )) - Args: $ARG"
 	else
-		echo "OK"
-	fi;
+		echo -e "$out\nOK"
+	fi
+}
+
+for i in {1..700000};
+do
+	testps $1 $2 $3 $4 &
 done
 
-echo "Max: $max - Min: $min - Diff: $(( $max - $min ))"
+wait
+
+# echo "Max: $max - Min: $min - Diff: $(( $max - $min ))"
