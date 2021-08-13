bb22a876c083bb7e08d8c713a7eee107036baef5
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-08-13 23:12:50 +0100

    Optimized output to use less actions

--- src/push_swap/optimizer.c
+++ src/push_swap/optimizer.c
@@ -6,7 +6,7 @@
 /*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 00:04:11 by mgueifao          #+#    #+#             */
-/*   Updated: 2021/08/09 00:17:00 by mgueifao         ###   ########.fr       */
+/*   Updated: 2021/08/12 02:27:06 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,73 +15,115 @@
 #include "ft_string.h"
 
 #include "actions.h"
+#include "push_swap.h"
 
-void	print_op_p(t_list **l)
+static int	check_next(t_list *l, char *op)
 {
-	char	*op;
-
-	if (!(*l)->next)
-		return ft_putstr_fd((*l)->content, 1);
-	if (ft_strcmp((*l)->content, "pa\n"))
-	{
-		op = (char *)(*l)->next->content;
-		if (!ft_strcmp(op, "rrb\n") || !(*l)->next->next)
-			return ft_putstr_fd("pa\n", 1);
-		op = (char *)(*l)->next->next->content;
-		if (!ft_strcmp(op, "pb\n"))
-			return ft_putstr_fd("pa\n", 1);
-		ft_putstr_fd("rrb\nsb\n", 1);
-		*l = (*l)->next->next;
-		return ;
-	}
-	op = (char *)(*l)->next->content;
-	if (!ft_strcmp(op, "rra\n") || !(*l)->next->next)
-		return ft_putstr_fd("pb\n", 1);
-	op = (char *)(*l)->next->next->content;
-	if (!ft_strcmp(op, "pa\n"))
-		return ft_putstr_fd("pb\n", 1);
-	ft_putstr_fd("rra\nsa\n", 1);
-	*l = (*l)->next->next;
+	if (ft_strcmp(op, "ra\n") || ft_strcmp(op, "rra\n"))
+		return ft_strcmp(l->content, "ra\n") || ft_strcmp(l->content, "sa\n")
+			|| ft_strcmp(l->content, "rra\n");
+	if (ft_strcmp(op, "rb\n") || ft_strcmp(op, "rrb\n"))
+		return ft_strcmp(l->content, "rb\n") || ft_strcmp(l->content, "sb\n")
+			|| ft_strcmp(l->content, "rrb\n");
+	return (0);
 }
 
-void	print_op_r(t_list **l)
+int	print_op_p(t_list **l)
+{
+	char	*op1;
+	char	*op2;
+
+	if ((*l)->next && ft_strcmp((*l)->content, "pa\n")
+		&& ft_strcmp((*l)->next->content, "rrb\n") && (*l)->next->next
+		&& ft_strcmp((*l)->next->next->content, "pb\n"))
+	{
+		op1 = "rrb\n";
+		op2 = "sb\n";
+	}
+	else if ((*l)->next && ft_strcmp((*l)->next->content, "rra\n")
+		&& (*l)->next->next && ft_strcmp((*l)->next->next->content, "pa\n"))
+	{
+		op1 = "rra\n";
+		op2 = "sa\n";
+	}
+	else
+		return (0);
+	replace_content(*l, op1);
+	replace_content((*l)->next, op2);
+	rem_next((*l)->next);
+	return (1);
+}
+
+int	print_op_r(t_list **l)
 {
 	t_list	*curr;
 
-	curr = (*l)->next;
-	while (curr && (ft_strcmp(curr->content, (*l)->content)
-		|| ft_strcmp(curr->content, "")))
+	curr = (*l);
+	while (curr->next && check_next(curr->next, (*l)->content))
 		curr = curr->next;
-	if (!(*l)->next)
-		return ft_putstr_fd((*l)->content, 1);
-	if ((ft_strcmp((*l)->content, "ra\n") && ft_strcmp(curr->content, "rb\n"))
+	if (curr->next && ((ft_strcmp((*l)->content, "ra\n")
+		&& ft_strcmp(curr->next->content, "rb\n"))
 		|| (ft_strcmp((*l)->content, "rb\n")
-		&& ft_strcmp(curr->content, "ra\n")))
+		&& ft_strcmp(curr->next->content, "ra\n"))))
 	{
-		ft_putstr_fd("rr\n", 1);
-		ft_free(curr->content);
-		curr->content = ft_strdup("");
-		*l = (*l)->next;
-		return ;
+		replace_content(*l, "rr\n");
+		rem_next(curr);
+		return (1);
 	}
-	ft_putstr_fd((*l)->content, 1);
+	return (0);
+}
+
+int	print_op_rr(t_list **l)
+{
+	t_list	*curr;
+
+	curr = (*l);
+	while (curr->next && check_next(curr->next, (*l)->content))
+		curr = curr->next;
+	if (curr->next && ((ft_strcmp((*l)->content, "rra\n")
+		&& ft_strcmp(curr->next->content, "rrb\n"))
+		|| (ft_strcmp((*l)->content, "rrb\n")
+		&& ft_strcmp(curr->next->content, "rra\n"))))
+	{
+		replace_content(*l, "rrr\n");
+		rem_next(curr);
+		return (1);
+	}
+	return (0);
 }
 
 void	optimize(t_ps *ps)
 {
 	t_list	*curr;
 	char	*op;
+	int		changed;
 
+	changed = 1;
+	while (changed)
+	{
+		changed = 0;
+		curr = ps->out;
+		while (curr)
+		{
+			op = curr->content;
+			if ((ft_strcmp(op, "pa\n") || ft_strcmp(op, "pb\n"))
+				&& print_op_p(&curr))
+				changed = 1;
+			else if ((ft_strcmp(op, "ra\n") || ft_strcmp(op, "rb\n"))
+				&& print_op_r(&curr))
+				changed = 1;
+			else if ((ft_strcmp(op, "rra\n") || ft_strcmp(op, "rrb\n"))
+				&& (print_op_rr(&curr)))
+				changed = 1;
+			curr = curr->next;
+		}
+		changed = changed || rem_dups(ps->out);
+	}
 	curr = ps->out;
 	while (curr)
 	{
-		op = curr->content;
-		if (ft_strcmp(op, "pa\n") || ft_strcmp(op, "pb\n"))
-			print_op_p(&curr);
-		else if (ft_strcmp(op, "ra\n") || ft_strcmp(op, "rb\n"))
-			print_op_r(&curr);
-		else
-			ft_putstr_fd(op, 1);
+		ft_putstr_fd(curr->content, 1);
 		curr = curr->next;
 	}
+	ft_lstclear(&(ps->out), ft_free);
 }
